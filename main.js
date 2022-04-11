70a7f40c6eb43e2133dc9bc64cdbaa8d0c7adac6
Kazz0704 <103437480+Kazz0704@users.noreply.github.com>
2022-04-11 22:58:29 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const conbtn1 = document.getElementById("conbtn1")
+const conbtn2 = document.getElementById("conbtn2")
+conbtn1.addEventListener("click", function () {
+    window.scroll(0, 0)
+})
+conbtn2.addEventListener("click", function () {
+    window.scroll(0, 10000)
+})
